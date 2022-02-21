84f42d5e9e7c033b92dc95aa9b671afee08b29b4
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-02-21 12:16:50 +0400

    fix: PureComponent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import styles from "./Header.module.scss";
 import * as images from "../../assets/images";
 
-export class Header extends Component {
+export class Header extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
