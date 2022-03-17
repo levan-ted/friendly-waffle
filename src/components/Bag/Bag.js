8d3333fa4872736bac6657ca7d2f7ad0fecf9c90
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-03-17 15:25:36 +0400

    feat: Render total price in products bag popup

--- src/components/Bag/Bag.js
+++ src/components/Bag/Bag.js
@@ -6,12 +6,20 @@ import styles from "./Bag.module.scss";
 import CartItem from "../CartItem";
 
 import { toggleBag } from "../../store/thunk";
+import { formatCurrency } from "../../helpers/format-currency";
 
 export class Bag extends Component {
   state = { showBag: false };
   render() {
     const quantity = this.props.cartItems.length;
-    const totalPrice = 100;
+    const totalPrice = this.props.cartItems.reduce((acc, item) => {
+      const quantity = item.quantity;
+      const price = item.prices.find(
+        (price) => price.currency.label === this.props.currency
+      ).amount;
+      const total = quantity * price;
+      return acc + total;
+    }, 0);
 
     return this.props.showBag ? (
       <div className={styles.wrapper}>
@@ -23,7 +31,12 @@ export class Bag extends Component {
           {this.props.cartItems.map((product) => {
             return <CartItem product={product} xs />;
           })}
-          <div className={styles.totals}>Total: {totalPrice} </div>
+          <div className={styles.totals}>
+            <span>Total</span>{" "}
+            <span>
+              {formatCurrency(totalPrice)} {this.props.currency}
+            </span>{" "}
+          </div>
           <div className={styles.buttons}>
             <Link to="/cart">
               <button onClick={this.props.toggleBag} className={styles.btn}>
@@ -44,6 +57,7 @@ export class Bag extends Component {
 
 const mapStateToProps = (state) => ({
   cartItems: state.cart.cartItems,
+  currency: state.currencies.active.label,
   showBag: state.ui.showBag,
 });
 
